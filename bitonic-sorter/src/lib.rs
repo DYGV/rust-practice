21cf60ea6922ede581e3d46540a3860364842a4b
unknown <dygv1188@gmail.com>
2020-01-17 19:37:16 +0900

    修正: 列挙型, match式

--- bitonic-sorter/src/lib.rs
+++ bitonic-sorter/src/lib.rs
@@ -1,2 +1,7 @@
 pub mod first;
 pub mod second;
+// 列挙型として昇順、降順を定義する
+pub enum SortOrder {
+    Ascending,
+    Descending,
+}
--- bitonic-sorter/src/second.rs
+++ bitonic-sorter/src/second.rs
@@ -1,4 +1,4 @@
-// Ordトレイトを実装した型ならなんでもソートできる
+// Ordトレイトを実装した型ならなんでもソートできるようなプログラム
 
 // publicな関数として他のモジュールからアクセスできることを示す
 // xの引数の型 = &はポインタ経由で借用することを示す
@@ -9,16 +9,26 @@
 // Rustでは関数、変数、定数にスネークケース、
 // ユーザが定義した型やジェネリクス型パラメータの識別子にはキャメルケースを用いる
 
+use super::SortOrder;
+
+// match式による場合分けをしてdo_sort()に渡す
+pub fn sort<T: Ord>(x: &mut [T], order: &SortOrder) {
+    match *order {
+        SortOrder::Ascending => do_sort(x, true),
+        SortOrder::Descending => do_sort(x, false),
+    };
+}
+
 // u32型のみに対応している
 // pub fn sort(x: &mut [u32], up: bool) {
 // 型パラメータTを導入して、関数をジェネリクス化する
 // 全順序だけを受け取るように、型パラメータTのトレイト境界としてOrdを設定
-pub fn sort<T: Ord>(x: &mut [T], up: bool) {
+fn do_sort<T: Ord>(x: &mut [T], up: bool) {
     // スライスの参照を受け取っているため値を返す必要がない
     if x.len() > 1 {
         let mid_point = x.len() / 2;
-        sort(&mut x[..mid_point], true);
-        sort(&mut x[mid_point..], false);
+        do_sort(&mut x[..mid_point], true);
+        do_sort(&mut x[mid_point..], false);
         sub_sort(x, up)
     }
 }
@@ -47,6 +57,7 @@ fn compare_and_swap<T: Ord>(x: &mut [T], up: bool) {
 mod tests {
     // 親モジュール(first)のsort関数を使用する
     use super::sort;
+    use crate::SortOrder::*;
 
     // テストケースになる関数は#[test]アトリビュートを付ける
     #[test]
@@ -60,7 +71,7 @@ mod tests {
 
         // xのスライスを作成し、sort関数を呼び出す
         // &mut xは&mut x[..]と書いてもよい
-        sort(&mut x, true);
+        sort(&mut x, &Ascending);
         assert_eq!(x, vec![4, 10, 11, 20, 21, 30, 110, 330]);
     }
 
@@ -68,7 +79,7 @@ mod tests {
     fn sort_u32_descending() {
         // xに型注釈Vec<u32>を付ける
         let mut x: Vec<u32> = vec![10, 30, 11, 20, 4, 330, 21, 110];
-        sort(&mut x, false);
+        sort(&mut x, &Descending);
         assert_eq!(x, vec![330, 110, 30, 21, 20, 11, 10, 4]);
     }
 
@@ -85,7 +96,7 @@ mod tests {
             "no",
             "GC",
         ];
-        sort(&mut x, true);
+        sort(&mut x, &Ascending);
         assert_eq!(
             x,
             vec![
@@ -113,7 +124,7 @@ mod tests {
             "that",
             "runs",
         ];
-        sort(&mut x, false);
+        sort(&mut x, &Descending);
         assert_eq!(
             x,
             vec![
